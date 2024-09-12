abdfcf9ff1a07cb0ba471c78fb5a5712e0aac590
Anshul Gupta <anshulgupta8854@gmail.com>
2024-09-12 23:27:57 +0530

    Initial Stage

---
+++ React/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Apps from "./COMPONENTS/Projects/First/Apps";
+
+function App() {
+    return (
+      <div>
+          <Apps></Apps>
+      </div>
+    );
+}
+
+export default App; 
